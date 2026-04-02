49bcf1d876ddb2bb6c1cccca57b3b8eb10b54327
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2026-04-02 10:27:32 +0800

    refactor: 🔥 cleanup

--- src/utils/gender.ts
+++ src/utils/gender.ts
@@ -1,8 +1,4 @@
-import { createContext } from './createContext'
-
 export enum Gender {
   FEMALE = 'F',
   MALE = 'M',
 }
-
-export const { Provider: genderProvider, Consumer: useGender } = createContext(Gender.FEMALE)
